d45016008421ae678602c600abf43bb672b13bf0
Administrador <Administrador@henrique_note>
2013-07-04 11:01:31 -0300

    Bugs resolvidos.

--- src/main/java/inscricao/faces/mngbeans/InscricaoBean.java
+++ src/main/java/inscricao/faces/mngbeans/InscricaoBean.java
@@ -2,12 +2,11 @@ package inscricao.faces.mngbeans;
 
 import inscricao.entity.Candidato;
 import inscricao.entity.Idioma;
-import inscricao.managed_beans.ListaCandidatos;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.model.SelectItem;
 import utfpr.faces.support.PageBean;
 
@@ -16,7 +15,7 @@ import utfpr.faces.support.PageBean;
  * @author Wilson
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class InscricaoBean extends PageBean {
     private static final Idioma[] idiomas = {
         new Idioma(1, "Inglês"),
@@ -25,7 +24,7 @@ public class InscricaoBean extends PageBean {
     };
     private Candidato candidato = new Candidato(idiomas[0]); // inicialmente ingles
     private List<SelectItem> idiomaItemList;
-    private ListaCandidatos listaCandidatos;
+    private boolean novoUsuario = true;
 
     public Candidato getCandidato() {
         return candidato;
@@ -45,22 +44,58 @@ public class InscricaoBean extends PageBean {
     }
 
     public String confirmaAction() {
-        listaCandidatos = new ListaCandidatos();
         candidato.setDataHora(new Date());
-        candidato.setIdioma(idiomas[candidato.getIdioma().getCodigo()-1]);
-        listaCandidatos.addCandidato(candidato);
-        return "confirma";
+        candidato.setIdioma(idiomas[candidato.getIdioma().getCodigo() - 1]);
+        ListaCandidatosBean listaCandidatos = (ListaCandidatosBean) getBean("listaCandidatosBean");
+        if (novoUsuario) {
+            if (candidatoValido()) {
+                listaCandidatos.adicionarCandidato(candidato);
+                return "confirma";
+            } else {
+                error("Cpf já cadastrado!");
+                return null;
+            }
+        } else {
+            listaCandidatos.alterarCandidato(candidato);
+            novoUsuario = true;
+            return "confirma";
+        }
     }
     
-    public String inscricaoAction() {
+    public boolean candidatoValido() {
+        ListaCandidatosBean listaCandidatos = (ListaCandidatosBean) getBean("listaCandidatosBean");
+        for (Candidato cand : listaCandidatos.getListaCandidatos()) {
+            if (candidato.getCpf() == cand.getCpf()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String novoAlterarUsuario() {
+        if (novoUsuario) {
+            return "Confirmar inscrição";
+        } else {
+            return "Alterar inscrição";
+        }
+    }
+    
+    public String limparCamposAction() {
+        novoUsuario = true;
+        candidato = new Candidato(idiomas[0]);
         return "inscricao";
     }
     
-    public String menuAction() {
-        return "menu";
+    public String alterarUsuarioAction() {
+        novoUsuario = false;
+        return "inscricao";
     }
     
-    public String alteraAction() {
-        return "inscricao";
+    public String getAlterar() {
+        if (novoUsuario) {
+            return "false";
+        } else {
+            return "true";
+        }
     }
 }
